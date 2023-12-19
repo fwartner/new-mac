a68e6cdc1dfffdc8dde11472af28f9b7173cc392
Florian Wartner <florian.wartner@cloud37.io>
2023-12-19 13:20:50 +0100

    Initial

--- .history/vscode_20231219131158.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Homebrew if not already installed
-if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Install Visual Studio Code
-brew install --cask visual-studio-code
